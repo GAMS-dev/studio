dfcb9c50ffc9cc8981abcf7365a1c80783bb6ef6
Alexander Fust <MrMontag@users.noreply.github.com>
2017-10-06 16:40:29 +0200

    55 internal mgmt (#12)
    Get GAMS system directory and additional MacOS zip artifact.

---
+++ src/gamsinfo.cpp
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the GAMS Studio project.
+ *
+ * Copyright (c) 2017 GAMS Software GmbH <support@gams.com>
+ * Copyright (c) 2017 GAMS Development Corp. <support@gams.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "gamsinfo.h"
+
+#include <QDir>
+#include <QFile>
+#include <QStandardPaths>
+#include <QMessageBox>
+
+namespace gams {
+namespace studio {
+
+GAMSInfo::GAMSInfo()
+{
+
+}
+
+QString GAMSInfo::systemDir() {
+#if defined(DISTRIB_BUILD) // For the GAMS distribution build
+#ifdef __linux__ // Linux AppImage
+    return QDir::currentPath().append("/..");
+#elif __APPLE__ // Apple MacOS dmg
+    auto path = QDir::currentPath();
+    QMessageBox::information(nullptr, "Path", path);
+    return path;
+#else // Windows
+    return QDir::currentPath().append("/..");
+#endif
+#else // Just a simple way for developers to find a GAMS distribution... if the PATH is set.
+    QMessageBox::information(nullptr, "Path", QStandardPaths::findExecutable("gams"));
+    return QFileInfo(QStandardPaths::findExecutable("gams")).absolutePath();
+#endif
+}
+
+}
+}
