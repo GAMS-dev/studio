fd617c5f7a3fb672c40d54cf23900d34df9c5508
Clemens <cwestphal@gams.com>
2017-10-17 15:42:45 +0200

    Create list of GDXSymbol objects

---
+++ src/gdxviewer/gdxsymbol.cpp
@@ -0,0 +1,14 @@
+#include "gdxsymbol.h"
+
+namespace gams {
+namespace studio {
+namespace gdxviewer {
+
+GDXSymbol::GDXSymbol()
+{
+
+}
+
+} // namespace gdxviewer
+} // namespace studio
+} // namespace gams
---
+++ src/gdxviewer/gdxsymbol.h
@@ -0,0 +1,19 @@
+#ifndef GDXSYMBOL_H
+#define GDXSYMBOL_H
+
+
+namespace gams {
+namespace studio {
+namespace gdxviewer {
+
+class GDXSymbol
+{
+public:
+    GDXSymbol();
+};
+
+} // namespace gdxviewer
+} // namespace studio
+} // namespace gams
+
+#endif // GDXSYMBOL_H
