0e9534fc7e8f32d8507fb606cce656053f58af69
Clemens <cwestphal@gams.com>
2017-12-11 19:03:38 +0100

    left click on fitler icon opens the column filter like right click does on whole column header

--- src/gdxviewer/gdxsymbolheaderview.cpp
+++ src/gdxviewer/gdxsymbolheaderview.cpp
@@ -2,6 +2,9 @@
 #include <QDebug>
 #include <QPainter>
 #include <QTableView>
+#include <QMouseEvent>
+#include <QFlags>
+#include <QApplication>
 
 #include "gdxsymbol.h"
 
@@ -12,10 +15,24 @@ namespace gdxviewer {
 GdxSymbolHeaderView::GdxSymbolHeaderView(Qt::Orientation orientation, QWidget *parent)
     : QHeaderView(orientation, parent)
 {
+    mFilterIconWidth = new int[GMS_MAX_INDEX_DIM + GMS_VAL_MAX];
+    mFilterIconX = new int[GMS_MAX_INDEX_DIM + GMS_VAL_MAX];
+    mFilterIconY = new int[GMS_MAX_INDEX_DIM + GMS_VAL_MAX];
+}
+
+GdxSymbolHeaderView::~GdxSymbolHeaderView()
+{
+    if(mFilterIconWidth)
+        delete mFilterIconWidth;
+    if(mFilterIconX)
+        delete mFilterIconX;
+    if(mFilterIconY)
+        delete mFilterIconY;
 }
 
 void GdxSymbolHeaderView::paintSection(QPainter *painter, const QRect &rect, int logicalIndex) const
 {
+    qDebug() << "paint";
     GdxSymbolHeaderView::QHeaderView::paintSection(painter, rect, logicalIndex);
 
     QTableView* tv = static_cast<QTableView*>(this->parent());
@@ -39,8 +56,33 @@ void GdxSymbolHeaderView::paintSection(QPainter *painter, const QRect &rect, int
         int posY = rect.bottomRight().y()-iconWidth-iconMargin;
 
         painter->drawImage(posX, posY, pm.toImage());
+
+        mFilterIconWidth[logicalIndex] = iconWidth;
+        mFilterIconX[logicalIndex] = posX;
+        mFilterIconY[logicalIndex] = posY;
+
         painter->save();
     }
+}
+
+void GdxSymbolHeaderView::mousePressEvent(QMouseEvent *event)
+{
+    if (Qt::LeftButton == event->button() && pointFilterIconCollision(event->pos()))
+        this->customContextMenuRequested(event->pos());
+    else
+        QHeaderView::mousePressEvent(event);
+}
+
+bool GdxSymbolHeaderView::pointFilterIconCollision(QPoint p)
+{
+    int index = logicalIndexAt(p);
+    if(p.x() >= mFilterIconX[index] && p.x() <= mFilterIconX[index]+mFilterIconWidth[index] &&
+       p.y() >= mFilterIconY[index] && p.y() <= mFilterIconY[index]+mFilterIconWidth[index])
+    {
+        return true;
+    }
+    return false;
+
 }
 
 } // namespace gdxviewer
--- src/gdxviewer/gdxsymbolheaderview.h
+++ src/gdxviewer/gdxsymbolheaderview.h
@@ -15,15 +15,23 @@ class GdxSymbolHeaderView : public QHeaderView
 
 public:
     GdxSymbolHeaderView(Qt::Orientation orientation, QWidget *parent=nullptr);
+    ~GdxSymbolHeaderView();
 
 protected:
     void paintSection(QPainter *painter, const QRect &rect, int logicalIndex) const override;
+    void mousePressEvent(QMouseEvent * event) override;
 
 private:
     QString iconFilterOn = ":/img/filter";
     QString iconFilterOff = ":/img/filter-off";
     const double ICON_SCALE_FACTOR = 0.5;
     const double ICON_MARGIN_FACTOR = 0.1;
+
+    int* mFilterIconWidth;
+    int* mFilterIconX;
+    int* mFilterIconY;
+
+    bool pointFilterIconCollision(QPoint p);
 };
 
 } // namespace gdxviewer
