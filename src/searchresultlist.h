e92c9827700207a4bcde600cad33955003a26ab4
Alexander Fust <afust@gams.com>
2018-01-23 16:04:02 +0100

    function override.

--- src/searchresultlist.h
+++ src/searchresultlist.h
@@ -42,7 +42,7 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
 
 private:
     QString mSearchTerm;
