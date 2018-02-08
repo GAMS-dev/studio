397062ae75f86209fc51953bf38aa247c2862207
MrMontag <afust@gams.com>
2018-02-08 11:03:35 +0100

    override warnings

--- src/option/optioncompleterdelegate.h
+++ src/option/optioncompleterdelegate.h
@@ -15,9 +15,9 @@ class OptionCompleterDelegate : public QStyledItemDelegate
 public:
     OptionCompleterDelegate(CommandLineTokenizer* tokenizer, QObject* parent = 0);
 
-    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-    void setEditorData(QWidget *editor, const QModelIndex &index) const;
-    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
 //    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelInd*/ex &index) const;
 
 protected:
