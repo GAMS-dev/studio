2b1e59eca6a9d380c23ee2c77f71c6ef69c25a59
MrMontag <afust@gams.com>
2018-03-15 16:49:35 +0100

    studio.rc update

--- src/version.h
+++ src/version.h
@@ -20,19 +20,19 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define FI_FILEVERSION             STUDIO_MAJOR_VERSION,STUDIO_MINOR_VERSION,STUDIO_PATCH_LEVEL,0
-#define FI_FILEVERSION_STR         "0.1.0.0\0"
+#define QUOTE_HELPER(m) #m
+#define QUOTE(m) QUOTE_HELPER(m)
+
+#define FI_VERSION                 STUDIO_MAJOR_VERSION,STUDIO_MINOR_VERSION,STUDIO_PATCH_LEVEL,0
+#define FI_VERSION_STR             QUOTE(STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL.0)
 #define FI_ORIGINALFILENAME_STR    "studio.exe"
 #define FI_INTERNALNAME_STR        "GAMS Studio"
 #define FI_PRODUCTNAME_STR         "GAMS Studio"
-#define FI_FILEDESCRIPTION_STR     "GAMS Studio - optimization modeling development"
+#define FI_FILEDESCRIPTION_STR     "GAMS Studio"
 
-#define FI_PRODUCTVERSION          STUDIO_MAJOR_VERSION,STUDIO_MINOR_VERSION,STUDIO_PATCH_LEVEL,00
-#define FI_PRODUCTVERSION_STR      "25.00.00.00"
-
-#define FI_COMPANYNAME_STR         "GAMS Software GmbH"
-#define FI_COMPANYDOMAIN_STR       "gams.com"
-#define FI_LEGALCOPYRIGHT_STR      "Copyright GAMS Software GmbH and GAMS Development Corp."
+#define FI_COMPANYNAME_STR         "GAMS Development Corp."
+#define FI_COMPANYDOMAIN_STR       "https://www.gams.com"
+#define FI_LEGALCOPYRIGHT_STR      "Copyright GAMS Software GmbH, Copyright GAMS Development Corp."
 #define FI_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define FI_LEGALTRADEMARKS2_STR    FI_LEGALTRADEMARKS1_STR
 
