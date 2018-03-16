b01330c24dca917ee4a5bdbfa21d907ddb6d4c21
Jarungjit Parnjai <Jarungjit Parnjai>
2018-03-16 16:12:16 +0100

    fixed bug in option editor: double click always takes option name column data

--- src/option/optioneditor.cpp
+++ src/option/optioneditor.cpp
@@ -233,10 +233,9 @@ void OptionEditor::showOptionContextMenu(const QPoint &pos)
 
 void OptionEditor::addOptionFromDefinition(const QModelIndex &index)
 {
-    QVariant data = optionDefinitionTreeView->model()->data(index);
+    QVariant data = optionDefinitionTreeView->model()->data(index.sibling(index.row(), OptionDefinitionModel::COLUMN_OPTION_NAME));
     QModelIndex defValueIndex = optionDefinitionTreeView->model()->index(index.row(), OptionDefinitionModel::COLUMN_DEF_VALUE);
     QModelIndex  parentIndex =  optionDefinitionTreeView->model()->parent(index);
-//    qDebug() <<  "parentIndex=" << parentIndex.row();
     // TODO insert before selected  or at the end when no selection
     commandLineTableView->model()->insertRows(commandLineTableView->model()->rowCount(), 1, QModelIndex());
     QModelIndex insertKeyIndex = commandLineTableView->model()->index(commandLineTableView->model()->rowCount()-1, 0);
