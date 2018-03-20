2217ca35aab221d6d069e603721fee22de078169
MrMontag <afust@gams.com>
2018-03-20 17:36:32 +0100

    warnings

--- src/lxiviewer/lxiviewer.cpp
+++ src/lxiviewer/lxiviewer.cpp
@@ -4,6 +4,7 @@
 #include "lxiviewer.h"
 #include "lxiparser.h"
 #include "lxitreemodel.h"
+#include "editors/codeeditor.h"
 #include "ui_lxiviewer.h"
 
 namespace gams {
@@ -11,8 +12,10 @@ namespace studio {
 namespace lxiviewer {
 
 LxiViewer::LxiViewer(CodeEditor *codeEditor, FileContext *fc, QWidget *parent):
-    QWidget(parent), mCodeEditor(codeEditor), mFileContext(fc),
-    ui(new Ui::LxiViewer)
+    QWidget(parent),
+    ui(new Ui::LxiViewer),
+    mCodeEditor(codeEditor),
+    mFileContext(fc)
 {
     ui->setupUi(this);
 
