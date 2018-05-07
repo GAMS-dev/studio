42d718449042154a488d37624acab83f0fbc2120
MrMontag <afust@gams.com>
2018-05-07 16:58:04 +0200

    logeditor.cpp refactorization.

--- src/editors/logeditor.cpp
+++ src/editors/logeditor.cpp
@@ -1,8 +1,8 @@
 /*
  * This file is part of the GAMS Studio project.
  *
- * Copyright (c) 2017 GAMS Software GmbH <support@gams.com>
- * Copyright (c) 2017 GAMS Development Corp. <support@gams.com>
+ * Copyright (c) 2017-2018 GAMS Software GmbH <support@gams.com>
+ * Copyright (c) 2017-2018 GAMS Development Corp. <support@gams.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include "logeditor.h"
-#include "keys.h"
+#include "studiosettings.h"
 
 namespace gams {
 namespace studio {
