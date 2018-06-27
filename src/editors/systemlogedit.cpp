082972c4938863d4a61cf576a2dfe02cc5472d05
Alexander Fust <afust@gams.com>
2018-06-27 13:56:46 +0200

    sys log highlighter

--- src/editors/systemlogedit.cpp
+++ src/editors/systemlogedit.cpp
@@ -7,12 +7,16 @@ namespace gams {
 namespace studio {
 
 SystemLogEdit::SystemLogEdit(StudioSettings *settings, QWidget *parent)
-    : AbstractEdit(settings, parent)
+    : AbstractEdit(settings, parent),
+      mHighlighter(new SystemLogHighlighter(this))
 {
-    setTextInteractionFlags(textInteractionFlags() | Qt::TextSelectableByKeyboard);
+    setTextInteractionFlags(textInteractionFlags() |
+                            Qt::TextSelectableByMouse |
+                            Qt::LinksAccessibleByMouse);
     setLineWrapMode(AbstractEdit::WidgetWidth);
     setFont(QFont(mSettings->fontFamily(), mSettings->fontSize()));
     setReadOnly(true);
+    mHighlighter->setDocument(document());
 }
 
 void SystemLogEdit::appendLog(const QString &msg, LogMsgType type)
@@ -22,21 +26,25 @@ void SystemLogEdit::appendLog(const QString &msg, LogMsgType type)
     QString logMsg;
     switch (type) {
     case LogMsgType::Info:
-        logMsg.append("<span style='color:blue;font-weight: bold;'>Info:</span> ");
+        logMsg.append("Info: ");
+        //logMsg.append("<span style='color:blue;font-weight: bold;'>Info:</span> ");
         break;
     case LogMsgType::Warning:
-        logMsg.append("<span style='color:orange;font-weight: bold;'>Warning:</span> ");
+        logMsg.append("Warning: ");
+        //logMsg.append("<span style='color:orange;font-weight: bold;'>Warning:</span> ");
         break;
     case LogMsgType::Error:
-        logMsg.append("<span style='color:red;font-weight: bold;'>Error:</span> ");
+        logMsg.append("Error: ");
+        //logMsg.append("<span style='color:red;font-weight: bold;'>Error:</span> ");
         break;
     }
 
-    logMsg.append(msg + "<br/>");
+    logMsg.append(msg);// + "<br/>");
 
-    moveCursor(QTextCursor::End);
-    appendHtml(logMsg);
-    moveCursor(QTextCursor::End);
+    appendPlainText(logMsg);
+//    moveCursor(QTextCursor::End);
+//    appendHtml(logMsg);
+//    moveCursor(QTextCursor::End);
 }
 
 AbstractEdit::EditorType SystemLogEdit::type()
--- src/editors/systemlogedit.h
+++ src/editors/systemlogedit.h
@@ -3,6 +3,11 @@
 
 #include "abstractedit.h"
 
+#include <QSyntaxHighlighter>
+#include <QRegularExpression>
+
+#include <QDebug>
+
 namespace gams {
 namespace studio {
 
@@ -10,14 +15,92 @@ class StudioSettings;
 
 enum class LogMsgType { Error, Warning, Info };
 
+class SystemLogHighlighter
+    : public QSyntaxHighlighter
+{
+    Q_OBJECT
+private:
+    struct HighlightingRule
+    {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+    };
+
+public:
+    SystemLogHighlighter(QObject *parent = nullptr)
+        : QSyntaxHighlighter(parent)
+    {
+//        QTextCharFormat errorFormat;
+//        errorFormat.setForeground(Qt::red);
+//        errorFormat.setFontWeight(QFont::Bold);
+//        HighlightingRule errorRule {
+//            QRegularExpression("Error:"),
+//            errorFormat
+//        };
+        //mHighlightingRules.push_back(errorRule);
+
+//        QTextCharFormat infoFormat;
+//        infoFormat.setForeground(Qt::darkBlue);
+//        infoFormat.setFontWeight(QFont::Bold);
+//        HighlightingRule infoRule {
+//            QRegularExpression("Info:"),
+//            infoFormat
+//        };
+        //mHighlightingRules.push_back(infoRule);
+
+//        QTextCharFormat warnFormat;
+//        warnFormat.setForeground(Qt::darkYellow);
+//        warnFormat.setFontWeight(QFont::Bold);
+//        HighlightingRule warnRule {
+//            QRegularExpression("Warning:"),
+//            warnFormat
+//        };
+        //mHighlightingRules.push_back(warnRule);
+
+        QTextCharFormat linkFormat;
+        linkFormat.setForeground(Qt::blue);
+        //linkFormat.setUnderlineColor(Qt::blue);
+        linkFormat.setUnderlineStyle(QTextCharFormat::SingleUnderline);
+        linkFormat.setAnchor(true);
+        //linkFormat.setAnchorName("???")
+        //linkFormat.setAnchorHref()
+        HighlightingRule linkRule {
+            QRegularExpression("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"),
+            linkFormat
+        };
+        mHighlightingRules.push_back(linkRule);
+    }
+
+protected:
+    void highlightBlock(const QString &text) override
+    {
+        QTextCursor tc(document());
+        for (HighlightingRule &rule : mHighlightingRules) {
+            QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
+            while (matchIterator.hasNext()) {
+                QRegularExpressionMatch match = matchIterator.next();
+                qDebug() << match.captured();
+                rule.format.setAnchorHref(match.captured());
+                rule.format.setAnchorName(match.captured());
+                setFormat(match.capturedStart(), match.capturedLength(), rule.format);
+            }
+        }
+    }
+
+private:
+    QVector<HighlightingRule> mHighlightingRules;
+};
+
 class SystemLogEdit : public AbstractEdit
 {
 public:
     SystemLogEdit(StudioSettings *settings, QWidget *parent);
     void appendLog(const QString &msg, LogMsgType type = LogMsgType::Warning);
 
-public:
     EditorType type() override;
+
+private:
+    SystemLogHighlighter *mHighlighter;
 };
 
 }
