c1261e321470472a9cc0e7cebd0545c1d8c3c0c0
Jarungjit Parnjai <Jarungjit Parnjai>
2018-07-24 12:47:49 +0200

    passing a single string of command line arguments on windows

--- src/gamsprocess.cpp
+++ src/gamsprocess.cpp
@@ -21,6 +21,7 @@
 
 #include <QStandardPaths>
 #include <QDir>
+#include <QDebug>
 #ifdef _WIN32
 #include "windows.h"
 #endif
@@ -56,14 +57,14 @@ void GamsProcess::setArguments(const QStringList &arguments)
 void GamsProcess::execute()
 {
     mProcess.setWorkingDirectory(mWorkingDir);
-    QStringList args({QDir::toNativeSeparators(mInputFile)});
+    QStringList args({"\""+QDir::toNativeSeparators(mInputFile)+"\""});
     args << "lo=3" << "ide=1" << "er=99" << "errmsg=1" << "pagesize=0" << "LstTitleLeftAligned=1";
     for(QString a : mArguments) {
         args << a;
     }
 #ifdef _WIN32
     mProcess.setNativeArguments(args.join(" "));
-    mProcess.start(nativeAppPath());
+    mProcess.start(nativeAppPath(), QStringList() << args.join(" "));
 #else // Linux and Mac OS X
     mProcess.start(nativeAppPath(), args);
 #endif
