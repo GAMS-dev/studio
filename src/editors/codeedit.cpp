197e86c92d5bb3448a63c2bbf6154e5e73193e24
Robin "Rogo" Goltermann <rgoltermann@gams.com>
2018-10-12 16:52:38 +0200

    fixed #774 word under cursor highlighting disappearing on scroll

--- src/editors/codeedit.cpp
+++ src/editors/codeedit.cpp
@@ -1188,12 +1188,14 @@ void CodeEdit::updateExtraSelections()
 
         QRegularExpressionMatch match = regexp.match(selectedText);
 
-        //    (  not caused by parenthiesis matching                                )
-        if ((((!extraSelMatchParentheses(selections, sender() == &mParenthesesDelay))
+        //   (  not caused by parenthiesis matching                               )
+        if (((!extraSelMatchParentheses(selections, sender() == &mParenthesesDelay)
               // ( depending on settings: no selection necessary OR has selection )
               && (mSettings->wordUnderCursor() || textCursor().hasSelection())
-              // (          wait for timer       ) OR (     always select          )
-              && ((sender() == &mParenthesesDelay) || (mSettings->wordUnderCursor()))))
+              // (      wait for timer            OR            user scrolled             )
+              && ((sender() == &mParenthesesDelay || sender() == this->verticalScrollBar())
+              //  OR (     always select          )
+                  || mSettings->wordUnderCursor())))
                 // AND deactivate when navigation search results
                 && match.captured(0).isEmpty())
             extraSelCurrentWord(selections);
