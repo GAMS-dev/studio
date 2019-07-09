b897927ea3dbcbb40b5988f010a902ba96e2ebeb
Jazzco <jmonki@gams.com>
2019-07-09 11:28:40 +0200

    fixes #1056

--- src/editors/codeedit.cpp
+++ src/editors/codeedit.cpp
@@ -52,7 +52,7 @@ CodeEdit::CodeEdit(QWidget *parent)
     connect(this, &CodeEdit::updateRequest, this, &CodeEdit::updateLineNumberArea);
     connect(this, &CodeEdit::cursorPositionChanged, this, &CodeEdit::recalcExtraSelections);
     connect(this, &CodeEdit::textChanged, this, &CodeEdit::recalcExtraSelections);
-    connect(this->verticalScrollBar(), &QScrollBar::valueChanged, this, &CodeEdit::updateExtraSelections);
+    connect(this->verticalScrollBar(), &QScrollBar::actionTriggered, this, &CodeEdit::updateExtraSelections);
     connect(document(), &QTextDocument::undoCommandAdded, this, &CodeEdit::undoCommandAdded);
 
     setMouseTracking(true);
