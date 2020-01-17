b5aa75009e87f86d4bca7cec26c3593f86cc46f5
afust <afust@gams.com>
2020-01-17 16:10:21 +0100

    improve checkbox behaviour of assembly file dialog

--- src/miro/miromodelassemblydialog.cpp
+++ src/miro/miromodelassemblydialog.cpp
@@ -22,7 +22,6 @@
 
 #include <QDir>
 #include <QMessageBox>
-#include <QDebug>
 
 namespace gams {
 namespace studio {
@@ -37,15 +36,13 @@ FilteredFileSystemModel::FilteredFileSystemModel(QObject *parent)
 bool FilteredFileSystemModel::filterAcceptsColumn(int source_column, const QModelIndex &source_parent) const
 {
     Q_UNUSED(source_parent)
-
-    if (source_column == 0)
-        return true;
-    return false;
+    return source_column == 0;
 }
 
 FileSystemModel::FileSystemModel(QObject *parent)
     : QFileSystemModel(parent)
 {
+    connect(this, &QFileSystemModel::directoryLoaded, this, &FileSystemModel::newDirectoryData);
 }
 
 QVariant FileSystemModel::data(const QModelIndex &idx, int role) const
@@ -54,8 +51,15 @@ QVariant FileSystemModel::data(const QModelIndex &idx, int role) const
         return QVariant();
 
     if (role == Qt::CheckStateRole) {
-        auto file = rootDirectory().relativeFilePath(filePath(idx));
-        if (mCheckedFiles.contains(file)) {
+        auto path = rootDirectory().relativeFilePath(filePath(idx));
+        if (isDir(idx)) {
+            int state = subdirectoryCheckState(path);
+            if (state == 1)
+                return Qt::PartiallyChecked;
+            if (state == 2)
+                return Qt::Checked;
+            return Qt::Unchecked;
+        } else if (mCheckedFiles.contains(path)) {
             return Qt::Checked;
         } else {
             return Qt::Unchecked;
@@ -67,15 +71,21 @@ QVariant FileSystemModel::data(const QModelIndex &idx, int role) const
 bool FileSystemModel::setData(const QModelIndex &idx, const QVariant &value, int role)
 {
     if (role == Qt::CheckStateRole && idx.column() == 0) {
+        updateChildDirInfo(idx);
         auto file = rootDirectory().relativeFilePath(filePath(idx));
         if (value.toBool()) {
             mCheckedFiles.insert(file);
             emit dataChanged(idx, idx);
+            if (isDir(idx))
+                updateChildSelection(idx);
+            addParentSelection(idx.parent());
             return true;
         } else {
-            auto pos = mCheckedFiles.find(file);
-            mCheckedFiles.erase(pos);
+            mCheckedFiles.erase(mCheckedFiles.find(file));
             emit dataChanged(idx, idx);
+            if (isDir(idx))
+                updateChildSelection(idx, true);
+            removeParentSelection(idx.parent());
             return true;
         }
     }
@@ -92,26 +102,30 @@ void FileSystemModel::selectAll()
     auto entries = rootDirectory().entryList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
     for (auto entry: entries) {
         auto idx = index(rootDirectory().absoluteFilePath(entry));
-        mCheckedFiles.insert(idx.data().toString());
-        emit dataChanged(idx, idx);
+        setData(idx, true, Qt::CheckStateRole);
     }
 }
 
 void FileSystemModel::clearSelection()
 {
-    auto checkedFiles = mCheckedFiles;
-    for (auto idx: checkedFiles) {
-        mCheckedFiles.remove(idx);
-        emit dataChanged(index(rootDirectory().absoluteFilePath(idx)),
-                         index(rootDirectory().absoluteFilePath(idx)));
-    }
+    beginResetModel();
+    mCheckedFiles.clear();
+    endResetModel();
 }
 
 QStringList FileSystemModel::selectedFiles()
 {
     QStringList selection;
-    for (auto file: mCheckedFiles)
-        selection << file;
+    for (auto file: mCheckedFiles) {
+        QFileInfo fileInfo(rootDirectory(), file);
+        if (fileInfo.isFile()) {
+            selection << file;
+        } else if (fileInfo.isDir()){
+            QDir dir(rootDirectory().absoluteFilePath(file));
+            if (dir.isEmpty())
+                selection << file;
+        }
+    }
     return selection;
 }
 
@@ -124,25 +138,149 @@ void FileSystemModel::setSelectedFiles(const QStringList &files)
     }
 }
 
+void FileSystemModel::newDirectoryData(const QString &path)
+{
+    auto checked = rootDirectory().relativeFilePath(path);
+    if (mCheckedFiles.contains(checked))
+        setData(index(path), true, Qt::CheckStateRole);
+}
+
+int FileSystemModel::checkedChilds(const QString &path) const
+{
+    int checkedChilds = 0;
+    if (isDir(index(path))) {
+        QDir dir(rootDirectory().absoluteFilePath(path));
+        for (auto info: dir.entryInfoList()) {
+            auto child = subPath(info.absoluteFilePath());
+            if (child == path)
+                continue;
+            if (!child.startsWith(path))
+                continue;
+            if (mCheckedFiles.contains(child))
+                ++checkedChilds;
+        }
+    }
+    return checkedChilds;
+}
+
+int FileSystemModel::directroyCheckState(const QString &path) const
+{
+    if (!mDirChilds.contains(path))
+        return 0;
+
+    int childs = mDirChilds[path];
+    int checked = checkedChilds(path);
+    if (checked>0 && checked<childs)
+        return 1; // partial
+    if (checked == childs && mCheckedFiles.contains(path))
+        return 2; // full
+    return 0; // unchecked
+}
+
+int FileSystemModel::subdirectoryCheckState(const QString &path) const
+{
+    QDirIterator iter(rootDirectory().absoluteFilePath(path),
+                      QDir::Dirs | QDir::NoDotAndDotDot);
+    while (iter.hasNext()) {
+        auto next = iter.next();
+        if (directroyCheckState(subPath(next)) == 1)
+            return 1; // partial
+    }
+    return directroyCheckState(path);
+}
+
+void FileSystemModel::updateChildDirInfo(const QModelIndex &idx)
+{
+    if (!isDir(idx))
+        return;
+
+    while (canFetchMore(idx))
+        fetchMore(idx);
+    auto path = subPath(idx);
+    mDirChilds[path] = rowCount(idx);
+}
+
+void FileSystemModel::updateParentDirInfo(const QModelIndex &parent)
+{
+    if (!parent.isValid())
+        return;
+
+    while (canFetchMore(parent))
+        fetchMore(parent);
+    auto path = subPath(parent);
+    mDirChilds[path] = rowCount(parent);
+
+    updateParentDirInfo(parent.parent());
+}
+
+void FileSystemModel::updateChildSelection(const QModelIndex &idx, bool remove)
+{
+    while (canFetchMore(idx))
+        fetchMore(idx);
+
+    for (int r=0; r<rowCount(idx); ++r) {
+        auto subIdx = index(r, 0, idx);
+        updateChildDirInfo(subIdx);
+
+        auto idxPath = rootDirectory().relativeFilePath(filePath(subIdx));
+        if (remove)
+            mCheckedFiles.remove(idxPath);
+        else
+            mCheckedFiles.insert(idxPath);
+
+        if (isDir(subIdx))
+            updateChildSelection(subIdx, remove);
+
+        emit dataChanged(subIdx, subIdx);
+    }
+}
+
+void FileSystemModel::addParentSelection(const QModelIndex &idx)
+{
+    if (!idx.isValid())
+        return;
+
+    updateParentDirInfo(idx);
+
+    auto path = subPath(idx);
+    if (path.isEmpty())
+        return;
+
+    mCheckedFiles.insert(path);
+    emit dataChanged(idx, idx);
+
+    addParentSelection(idx.parent());
+}
+
+void FileSystemModel::removeParentSelection(const QModelIndex &idx)
+{
+    if (!idx.isValid())
+        return;
+
+    auto path = rootDirectory().relativeFilePath(filePath(idx));
+    mCheckedFiles.remove(path);
+    removeParentSelection(idx.parent());
+    emit dataChanged(idx, idx);
+}
+
+QString FileSystemModel::subPath(const QModelIndex &idx) const
+{
+    return filePath(idx).remove(0, rootDirectory().canonicalPath().size()+1);
+}
+
+QString FileSystemModel::subPath(const QString &path) const
+{
+    return QString(path).remove(0, rootDirectory().canonicalPath().size()+1);
+}
+
 MiroModelAssemblyDialog::MiroModelAssemblyDialog(const QString &workingDirectory, QWidget *parent)
     : QDialog(parent),
       ui(new Ui::MiroModelAssemblyDialog),
-      mFileSystemModel(new FileSystemModel(this))
+      mWorkingDirectory(workingDirectory)
 {
     ui->setupUi(this);
     setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
-
-    mFileSystemModel->setRootPath(workingDirectory);
-    auto filterModel = new FilteredFileSystemModel(this);
-    filterModel->setSourceModel(mFileSystemModel);
-
-    auto oldModel = ui->directoryView->selectionModel();
-    ui->directoryView->setModel(filterModel);
-    delete oldModel;
-
-    auto rootIndex = mFileSystemModel->index(workingDirectory);
-    ui->directoryView->setRootIndex(filterModel->mapFromSource(rootIndex));
-    ui->directoryView->expandAll();
+    setupViewModel();
 }
 
 MiroModelAssemblyDialog::~MiroModelAssemblyDialog()
@@ -152,26 +290,53 @@ MiroModelAssemblyDialog::~MiroModelAssemblyDialog()
 
 QStringList MiroModelAssemblyDialog::selectedFiles()
 {
-    return mFileSystemModel->selectedFiles();
+    if (mFileSystemModel)
+        return mFileSystemModel->selectedFiles();
+    return QStringList();
 }
 
 void MiroModelAssemblyDialog::on_createButton_clicked()
 {
-    if (selectedFiles().isEmpty()) {
-        showMessageBox();
+    if (!mFileSystemModel)
         return;
-    }
-    accept();
+    if (selectedFiles().isEmpty())
+        showMessageBox();
+    else
+        accept();
 }
 
 void MiroModelAssemblyDialog::on_selectAllButton_clicked()
 {
-    mFileSystemModel->selectAll();
+    if (mFileSystemModel)
+        mFileSystemModel->selectAll();
 }
 
 void MiroModelAssemblyDialog::on_clearButton_clicked()
 {
-    mFileSystemModel->clearSelection();
+    if (mFileSystemModel) {
+        mFileSystemModel->clearSelection();
+        auto rootIndex = mFileSystemModel->index(mWorkingDirectory);
+        ui->directoryView->setRootIndex(mFilterModel->mapFromSource(rootIndex));
+    }
+}
+
+void MiroModelAssemblyDialog::setupViewModel()
+{
+    if (mWorkingDirectory.isEmpty())
+        return;
+
+    mFileSystemModel = new FileSystemModel(this);
+    mFileSystemModel->setRootPath(mWorkingDirectory);
+    mFilterModel = new FilteredFileSystemModel(this);
+    mFilterModel->setSourceModel(mFileSystemModel);
+
+    auto oldModel = ui->directoryView->selectionModel();
+    ui->directoryView->setModel(mFilterModel);
+    delete oldModel;
+
+    auto rootIndex = mFileSystemModel->index(mWorkingDirectory);
+    ui->directoryView->setRootIndex(mFilterModel->mapFromSource(rootIndex));
+    ui->directoryView->expandAll();
 }
 
 void MiroModelAssemblyDialog::showMessageBox()
