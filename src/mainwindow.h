c2ec7c2164ebef75d7289af7ebcec3d48fe38426
MrMontag <afust@gams.com>
2020-03-24 14:24:41 +0100

    no gams/miro exection if path is invalid

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -213,7 +213,6 @@ private slots:
     void storeTree();
     void cloneBookmarkMenu(QMenu *menu);
     void editableFileSizeCheck(const QFile &file, bool &canOpen);
-    void updateMiroMenu();
     void newProcessCall(const QString &text, const QString &call);
 
     // View
@@ -262,6 +261,7 @@ private slots:
     void on_actionStop_MIRO_triggered();
     void on_actionCreate_model_assembly_triggered();
     void on_actionDeploy_triggered();
+    void on_menuMIRO_aboutToShow();
     void miroDeploy(bool testDeploy, miro::MiroDeployMode mode);
     void setMiroRunning(bool running);
 
@@ -382,6 +382,8 @@ private:
     void updateToolbar(QWidget* current);
     void deleteScratchDirs(const QString& path);
     QFont createEditorFont(const QString &fontFamily, int pointSize);
+    bool isMiroAvailable();
+    bool validMiroPrerequisites();
 
 private:
     Ui::MainWindow *ui;
