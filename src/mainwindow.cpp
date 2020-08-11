d227fb2f97593c102d5b32d7f6a26423463ddf68
Robin "Rogo" Goltermann <rgoltermann@gams.com>
2020-08-11 16:27:42 +0200

    fixed missing tab browser button when studio starts maximized/fullscreen (#1532)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2223,9 +2223,9 @@ void MainWindow::restoreFromSettings()
 
     mMaximizedBeforeFullScreen = settings->toBool(skWinMaximized);
     if (settings->toBool(skWinFullScreen)) {
-        showFullScreen();
+        setWindowState(windowState() ^ Qt::WindowFullScreen);
     } else if (mMaximizedBeforeFullScreen) {
-        showMaximized();
+        setWindowState(windowState() ^ Qt::WindowMaximized);
     }
     ui->actionFull_Screen->setChecked(settings->toBool(skWinFullScreen));
     restoreState(settings->toByteArray(skWinState));
