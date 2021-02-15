d806377b8a6ce85c99c97f9ec58b085c7f13b664
Mohamed Ali Dekhil <dekhil.medali@gmail.com>
2021-02-15 17:01:12 +0000

    added/updated copyright statement

--- src/gdxviewer/valuefilter.cpp
+++ src/gdxviewer/valuefilter.cpp
@@ -1,8 +1,8 @@
 /*
  * This file is part of the GAMS Studio project.
  *
- * Copyright (c) 2017-2019 GAMS Software GmbH <support@gams.com>
- * Copyright (c) 2017-2019 GAMS Development Corp. <support@gams.com>
+ * Copyright (c) 2017-2021 GAMS Software GmbH <support@gams.com>
+ * Copyright (c) 2017-2021 GAMS Development Corp. <support@gams.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
