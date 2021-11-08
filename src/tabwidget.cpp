910c996040db99e8436bf8b60a2841b1c1f50e62
Jesko von Monkiewitsch <j.monkiewitsch@gmail.com>
2021-11-08 15:07:27 +0100

    Fixed wheel behavior in tabs

--- src/tabwidget.cpp
+++ src/tabwidget.cpp
@@ -9,7 +9,12 @@ namespace studio {
 TabWidget::TabWidget(QWidget *parent)
     : QTabWidget(parent)
 {
-
+    setUsesScrollButtons(true);
+    for (QObject *obj : tabBar()->children()) {
+        QToolButton *tb = qobject_cast<QToolButton*>(obj);
+        if (tb) (bLeft ? bRight : bLeft) = tb;
+    }
+    tabBar()->installEventFilter(this);
 }
 
 void TabWidget::mouseReleaseEvent(QMouseEvent *event)
@@ -22,6 +27,23 @@ void TabWidget::mouseReleaseEvent(QMouseEvent *event)
     }
 
     QTabWidget::mouseReleaseEvent(event);
+}
+
+bool TabWidget::eventFilter(QObject *sender, QEvent *event)
+{
+    Q_UNUSED(sender)
+    if (event->type() == QEvent::Wheel) {
+        QWheelEvent *we = static_cast<QWheelEvent*>(event);
+        if (!we->modifiers().testFlag(Qt::ControlModifier) && bLeft && bRight) {
+            if (we->angleDelta().y() > 0) {
+                if (!bLeft->isHidden()) bLeft->clicked();
+            } else {
+                if (!bRight->isHidden()) bRight->clicked();
+            }
+            return true;
+        }
+    }
+    return false;
 }
 
 }
