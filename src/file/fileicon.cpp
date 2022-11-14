b5f084dd33ece31dd4f9a907e539ff047611fcda
Jarungjit Parnjai <jparnjai@gams.com>
2022-11-14 15:44:09 +0100

    add gams-connect icon

--- src/file/fileicon.cpp
+++ src/file/fileicon.cpp
@@ -35,7 +35,7 @@ QIcon FileIcon::iconForFileKind(FileKind kind, bool isReadonly, bool isMain, QIc
     if (kind == FileKind::Gdx) return Theme::icon(":/img/database", mode, alpha);
     if (kind == FileKind::Ref) return Theme::icon(":/img/ref-file", mode, alpha);
     if (kind == FileKind::Opt) return Theme::icon(":/img/option-file", mode, alpha);
-    if (kind == FileKind::GCon) return Theme::icon(":/img/file-edit", mode, alpha);
+    if (kind == FileKind::GCon) return Theme::icon(":/img/file-connect", mode, alpha);
     if (kind == FileKind::Guc) return Theme::icon(":/img/gams-config-file", mode, alpha);
     if (kind == FileKind::Lst) return Theme::icon(":/img/file-alt", mode, alpha);
     if (kind == FileKind::Lxi) return Theme::icon(":/img/file-alt", mode, alpha);
