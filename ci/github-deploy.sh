0fb48113a2906cd3c0b945a1169486843680b106
Alexander Fust <afust@gams.com>
2022-11-16 17:04:36 +0000

    GitLab pipeline

---
+++ ci/github-deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+
+set +e
+
+## get version information for artifact, tag name and description
+GAMS_DISTRIB_MAJOR_VERSION=$(grep ^GAMS_DISTRIB_MAJOR version | cut -f2 -d"=")
+GAMS_DISTRIB_MINOR_VERSION=$(grep ^GAMS_DISTRIB_MINOR version | cut -f2 -d"=")
+export GAMS_DISTRIB_VERSION=$GAMS_DISTRIB_MAJOR_VERSION.$GAMS_DISTRIB_MINOR_VERSION
+
+## get changelog content
+CHANGELOG=""
+foundFirst="false"
+while IFS="" read -r line
+do
+    if [[ $line =~ ^Version.* ]] && [[ $foundFirst == "true" ]]; then
+        break
+    elif [[ $line =~ ^Version.* ]]; then
+        foundFirst="true"
+    elif [[ $line =~ ^=.* ]]; then
+    	 continue
+    else
+        CHANGELOG+=$line$'\n'
+    fi
+done < CHANGELOG
+
+## GitHub parameters
+GITHUB_ORGA=GAMS-dev
+GITHUB_REPO=studio
+
+## delete old release if needed
+RELEASE_DELETED=0
+github-release delete --user $GITHUB_ORGA --repo $GITHUB_REPO --tag ${TAG_NAME} && RELEASE_DELETED=0 || RELEASE_DELETED=1
+if [[ $RELEASE_DELETED -eq 0 ]]; then
+  echo "Release deleted"
+else
+  echo "No release to delete"
+
+## create a GitHub release
+if [[ ${TAG_NAME} =~ ^v[0-9]+\\.[0-9]+\\.[0-9]+$ ]]; then
+    github-release release --user $GITHUB_ORGA --repo $GITHUB_REPO --tag ${TAG_NAME} \
+--description "${CHANGELOG}
+
+This new version of GAMS Studio requires GAMS ${GAMS_DISTRIB_VERSION} or higher. To download GAMS, please visit https://www.gams.com/latest/. To learn more about GAMS Studio, please visit https://www.gams.com/latest/docs/T_STUDIO.html"
+elif [[ ${TAG_NAME} =~ ^v[0-9]+\\.[0-9]+\\.[0-9]+-rc\\.[0-9]+$ ]]; then
+    github-release release --user $GITHUB_ORGA --repo $GITHUB_REPO --tag ${TAG_NAME} --pre-release \
+--description "${CHANGELOG}
+
+This new version of GAMS Studio requires GAMS ${GAMS_DISTRIB_VERSION} or higher. To download GAMS, please visit https://www.gams.com/latest/. To learn more about GAMS Studio, please visit https://www.gams.com/latest/docs/T_STUDIO.html"
+fi
+
+## upload artifacts to GitHub
+if [[ ${TAG_NAME} =~ ^v[0-9]+\\.[0-9]+\\.[0-9]+$ ]]; then
+    cd artifacts
+    mv $(find *dmg) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.dmg"
+    mv $(find *AppImage) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.AppImage"
+    mv $(find *zip) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.zip"
+    ls -al
+    parallel github-release --verbose upload -R --user $GITHUB_ORGA --repo $GITHUB_REPO --tag ${TAG_NAME} --name {} --file {} ::: *.*
+fi
+
