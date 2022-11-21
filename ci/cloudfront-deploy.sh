47e21e7931397fe7a0339ea65074b2e3844de88f
Alexander Fust <afust@gams.com>
2022-11-21 09:56:38 +0000

    GitLab CI fixes

--- ci/cloudfront-deploy.sh
+++ ci/cloudfront-deploy.sh
@@ -9,9 +9,9 @@ STUDIO_PATCH_LEVEL=$(grep ^STUDIO_PATCH_LEVEL version | cut -f2 -d"=")
 
 ## change to artifact location and name
 cd artifacts
-mv $(find *dmg) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.dmg"
-mv $(find *AppImage) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.AppImage"
-mv $(find *zip) "GAMS_Studio-$STUDIO_MAJOR_VERSION.STUDIO_MINOR_VERSION.STUDIO_PATCH_LEVEL-x86_64.zip"
+mv $(find *dmg) "GAMS_Studio-$STUDIO_MAJOR_VERSION.$STUDIO_MINOR_VERSION.$STUDIO_PATCH_LEVEL-x86_64.dmg"
+mv $(find *AppImage) "GAMS_Studio-$STUDIO_MAJOR_VERSION.$STUDIO_MINOR_VERSION.$STUDIO_PATCH_LEVEL-x86_64.AppImage"
+mv $(find *zip) "GAMS_Studio-$STUDIO_MAJOR_VERSION.$STUDIO_MINOR_VERSION.$STUDIO_PATCH_LEVEL-x86_64.zip"
 
 ## folder name which matches the current tag, like v0.10.2 or v0.11.0-rc
 FOLDER_NAME="${TAG_NAME}"
@@ -35,7 +35,7 @@ s3cmd ls --no-check-certificate -r $URL --access_key=${S3_ACCESS_KEY} --secret_k
 cd ..
 
 ## Update gams.com for final releases
-if [[ ${TAG_NAME} =~ ^v[0-9]+\\.[0-9]+\\.[0-9]+$ ]]; then
+if [[ ${TAG_NAME} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     ## prepare GAMS Studio version line
     DATE=$(date +"%y%m%d")
     LINE="[$STUDIO_MAJOR_VERSION$STUDIO_MINOR_VERSION$STUDIO_PATCH_LEVEL.$STUDIO_MAJOR_VERSION.$STUDIO_MINOR_VERSION.$STUDIO_PATCH_LEVEL,$DATE]"
