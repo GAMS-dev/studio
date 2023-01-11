f452b42b46d0e767cd7518db753b357045e7fe4e
Jazzco <jmonki@gams.com>
2023-01-11 11:37:49 +0100

    revert warning fix

--- src/connect/clickablelabel.cpp
+++ src/connect/clickablelabel.cpp
@@ -23,10 +23,10 @@ namespace gams {
 namespace studio {
 namespace connect {
 
-ClickableLabel::ClickableLabel(QWidget *parent)
+ClickableLabel::ClickableLabel(QWidget *parent, Qt::WindowFlags f)
     : QLabel(parent) {  }
 
-ClickableLabel::ClickableLabel(const QString &text, QWidget *parent)
+ClickableLabel::ClickableLabel(const QString &text, QWidget *parent, Qt::WindowFlags f)
     : QLabel(parent)
 {
     setText(text);
