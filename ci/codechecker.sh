63ab57941ddf6725f2f2584848b3d56506144dc8
Alexander Fust <afust@gams.com>
2023-07-26 17:23:43 +0000

    ci code analysis

---
+++ ci/codechecker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+qmake gams-studio.pro CONFIG+=release
+
+# Log your project.
+CodeChecker log -b "make -j4" -o compilation_database.json
+
+# disable --ctu and clangsa because it is to slow
+CodeChecker analyze                              \
+  --analyzers clang-tidy                         \
+  -i ci/skipfile.txt                             \
+  -o reports                                     \
+  compilation_database.json
+
+# Create the report file by using the CodeChecker parse command.
+CodeChecker parse \
+  --trim-path-prefix $(pwd) \
+  -e codeclimate \
+  reports > gl-code-quality-report.json
+
+exit 0
