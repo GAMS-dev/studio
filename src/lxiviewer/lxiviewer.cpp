20638fdb2bbfd8198cad890e75708e851361a687
Jesko von Monkiewitsch <jmonki@gams.com>
2024-02-05 13:38:52 +0000

    Fix crash on lxiViewer with invalid model index

--- src/lxiviewer/lxiviewer.cpp
+++ src/lxiviewer/lxiviewer.cpp
@@ -127,10 +127,14 @@ void LxiViewer::loadLxi()
         LxiTreeModel* oldModel = static_cast<LxiTreeModel*>(ui->lxiTreeView->model());
         ui->lxiTreeView->setModel(model);
         QModelIndex idx = model->index(0,0);
-        ui->lxiTreeView->setCurrentIndex(idx);
-        markLine(idx);
-        if (oldModel)
-            delete oldModel;
+        if (idx.isValid()) {
+            ui->lxiTreeView->setCurrentIndex(idx);
+            markLine(idx);
+            if (oldModel)
+                delete oldModel;
+        } else {
+            ui->splitter->widget(0)->hide();
+        }
     }
     else
         ui->splitter->widget(0)->hide();
