5613c23956d3baefaf0ceb780324944662844c43
Alexander Fust <afust@gams.com>
2024-04-09 11:57:19 +0000

    upgrade to Qt 6.6.3

--- ci/codechecker.sh
+++ ci/codechecker.sh
@@ -5,16 +5,17 @@ qmake gams-studio.pro CONFIG+=release
 # Log your project.
 CodeChecker log -b "make -j4" -o compilation_database.json
 
-CodeChecker analyze                       \
-  --analyzers clang-tidy                  \
-  -i ci/skipfile.txt                             \
-  -o reports                                     \
+CodeChecker analyze           \
+  --analyzers clang-tidy      \
+  --jobs 4                    \
+  -i ci/skipfile.txt          \
+  -o reports                  \
   compilation_database.json
 
 # Create the report file by using the CodeChecker parse command.
-CodeChecker parse \
-  --trim-path-prefix $(pwd) \
-  -e codeclimate \
+CodeChecker parse             \
+  --trim-path-prefix $(pwd)   \
+  -e codeclimate              \
   reports > gl-code-quality-report.json
 
 exit 0
