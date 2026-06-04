b41363214ce3dfe56fd4c3bb748e0d57785cea80
Jarungjit Parnjai <jparnjai@gams.com>
2026-06-04 14:13:25 +0200

    remove unused code

--- src/application.cpp
+++ src/application.cpp
@@ -94,8 +94,6 @@ Application::Application(int& argc, char** argv)
     userName = qEnvironmentVariable("USERNAME", QString());
 #endif
     mServerName = "com.gams.studio." + userName;
-    QPalette pal = palette();
-    pal.setColor(QPalette::Disabled, QPalette::Window, QColor(255,255,255));
 
     connect(&mServer, &QLocalServer::newConnection, this, &Application::newConnection);
 }
