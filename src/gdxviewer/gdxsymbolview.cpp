6ad5c42f70f90dba603d9da7c709dc7d6bc90159
Clemens Westphal <cwestphal@gams.com>
2026-07-01 16:37:25 +0200

    Prevent crash

--- src/gdxviewer/gdxsymbolview.cpp
+++ src/gdxviewer/gdxsymbolview.cpp
@@ -1317,6 +1317,13 @@ void GdxSymbolView::onSearch(bool backward, bool startAtCurrent)
     bool first = true;
     while (true) {
         auto iter = vToL.find(visualCol);
+
+        if (iter == vToL.end()) {
+            iter = vToL.lowerBound(visualCol);
+            if (iter == vToL.end())
+                iter = vToL.begin();
+        }
+
         if (backward) {
             if (iter == vToL.begin()) {
                 iter = vToL.end();
